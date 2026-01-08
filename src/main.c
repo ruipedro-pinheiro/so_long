d9d61c430aeeb340e5c384466cfdc5c0457d4b92
ruipedro-pinheiro <pedroyverdon@gmail.com>
2026-01-08 19:35:36 +0100

    Added basic code that will show one red pixel on a window.
    TODO: Learn how to make lines, how to make geometric forms or even how
    to show xpm or png images. This is also very basic so there is no key
    working to exit the program than going in the terminal and make Ctrl +
    C.

--- src/main.c
+++ src/main.c
@@ -6,19 +6,46 @@
 /*   By: rpinheir <rpinhier@student.42Lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 12:30:47 by rpinheir          #+#    #+#             */
-/*   Updated: 2026/01/06 13:15:31 by rpinheir         ###   ########.ch       */
+/*   Updated: 2026/01/08 19:31:54 by rpinheir         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+void		my_mlx_pixel_put(t_data *data, int x, int y, int color);
+
 int	main(void)
 {
-	char	*mlx;
-	char	*mlx_win;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	*img;
 
-	// write(1, "Map is not given in argument\n", 5);
+	img = malloc(sizeof(t_data));
+	if (!img)
+		return (1);
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	img->img = mlx_new_image(mlx, 1920, 1080);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel,
+			&img->line_length, &img->endian);
+	my_mlx_pixel_put(img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img->img, 0, 0);
 	mlx_loop(mlx);
+	return (0);
+}
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
